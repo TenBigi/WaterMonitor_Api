08af2cc8655f21c7ea359656e8ed7c3f8afb50f7
TenBigi <jeniksoffer@gmail.com>
2024-06-30 23:15:02 +0200

    Přidejte soubory projektu.

---
+++ Program.cs
@@ -0,0 +1,95 @@
+﻿
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+using System;
+using System.Text;
+using vosplzen.sem2._2023.apiClient;
+using vosplzen.sem2._2023.apiClient.Contracts;
+
+internal class Program
+{
+    private static async Task Main(string[] args)
+    {
+        var builder = new ConfigurationBuilder();
+        builder.SetBasePath(Directory.GetCurrentDirectory())
+               .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+
+        IConfiguration config = builder.Build();
+
+        string connectionString = config["Sql:ConnectionString"];
+
+        string apiBaseUri = config["Api:BaseUri"];
+        string token = config["Api:AuthToken"];
+
+        string logFolder = config["App:LogFolder"];
+        string errorFolder = config["App:ErrorFolder"];
+
+        while (true)
+        {
+            await SendValues(config);
+            await Task.Delay(TimeSpan.FromSeconds(5));
+        }
+    }
+
+    public static async Task SendValues(IConfiguration config)
+    {
+        string apiBaseUri = config["Api:BaseUri"];
+
+        string tokenEndpoint = $"{apiBaseUri}/api/get-latest-token";
+        string authToken = await GetLatestTokenAsync(tokenEndpoint);
+
+        var message = GenerateFloodReport();
+
+        using (var apiclient = new ApiClient())
+        {
+            await apiclient.SendMessage(message, apiBaseUri, authToken);
+        }
+    }
+
+    public static async Task<string> GetLatestTokenAsync(string tokenEndpoint)
+    {
+        using (HttpClient client = new())
+        {
+            var response = await client.GetAsync(tokenEndpoint);
+
+            if (response.IsSuccessStatusCode)
+            {
+                var token = await response.Content.ReadAsStringAsync();
+                return token;
+            }
+            else
+            {
+                throw new Exception($"Failed to retrieve latest token. Status code: {response.StatusCode}");
+            }
+        }
+    }
+
+    public static FloodReport GenerateFloodReport()
+    {
+        Random random = new Random();
+        int value = (random.Next(1,100));
+        int stationId = 3;
+        DateTime now = DateTime.Now;
+        DateTime oneYearAgo = now.AddYears(-1);
+
+        DateTime dateTime = new DateTime(now.Year, random.Next(1, 13), random.Next(1, 29), random.Next(0, 24), random.Next(0, 60), random.Next(0, 60));
+
+        if (dateTime > now)
+        {
+            dateTime = now;
+        }
+        else if (dateTime < oneYearAgo)
+        {
+            dateTime = oneYearAgo;
+        }
+
+        FloodReport floodReport = new FloodReport
+        {
+            StationId = stationId,
+            TimeStamp = dateTime,
+            Value = value
+        };
+
+        return floodReport;
+    }
+}
---
+++ apiclient.cs
@@ -0,0 +1,56 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using vosplzen.sem2._2023.apiClient.Contracts;
+
+namespace vosplzen.sem2._2023.apiClient
+{
+
+    public class ApiClient : IDisposable
+    {
+        private HttpClient httpClient;
+
+        public ApiClient()
+        {
+            httpClient = new HttpClient();
+        }
+       
+        public async Task<bool> SendMessage(object message, string baseurl, string authToken)
+        {
+            try
+            {
+                var settings = new Newtonsoft.Json.JsonSerializerSettings() { Culture = new CultureInfo("cs-CZ") };
+                var json = Newtonsoft.Json.JsonConvert.SerializeObject(message, Formatting.Indented, settings);
+
+                Console.WriteLine("message:");
+                Console.WriteLine(json);
+
+                var data = new StringContent(json, Encoding.UTF8, "application/json");
+                httpClient.DefaultRequestHeaders.Add("Security-Token", authToken);
+
+                var response = await httpClient.PostAsync($@"{baseurl}/api/recieve-measurements", data);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    Console.WriteLine("Message sent successfully.");
+                    return true;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Failed to send data: {ex.Message}");
+            }
+
+            return false;
+        }
+
+        public void Dispose()
+        {
+            httpClient.Dispose();
+        }
+    }
+}
